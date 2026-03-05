2b185dc9e1458c3464598d223ec75344dcb4c2cf
bhakti-0201 <bhaktii.0201@gmail.com>
2026-03-05 16:57:51 +0530

    Add product model

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+namespace InventorySystem.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+    }
+}
